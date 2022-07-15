4bb935732f509004ceeddeb6f20dfe9127640e49
Klaudia Sagan <klaudiasagan.code@gmail.com>
2022-07-15 10:57:14 +0200

    creating functions for operators

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const buttonsContainer = document.querySelector(`section.buttons`);
 
 const screenElement = document.querySelector('section.screen div');
 
+const equalsButton = document.querySelector('[data-equals]');
+
+const deleteButton = document.querySelector('[data-delete]');
+
+const allClearButton = document.querySelector('[data-all-clear]');
+
 const orderArray = [];
 
-const operatorExists = false;
+let operatorExists = false;
 
 buttonsContainer.addEventListener('click', function(eventObject) {
 
@@ -20,10 +26,38 @@ buttonsContainer.addEventListener('click', function(eventObject) {
   if (!isNaN(parsedValue)){
     if(!operatorExists){
       orderArray[0] = parseFloat(currentScreenText + eventObject.target.textContent);
+    } else {
+       orderArray[2] = parseFloat( (orderArray[2] ? orderArray[2] : '') + eventObject.target.textContent);
     }
+  } else {
+     if (!operatorExists){
+       orderArray.push(eventObject.target.textContent);
+       operatorExists = true;
+     } else {
+        // tu będziemy liczyć
+        const calculationResult = calculate (orderArray);
+        screenElement.textContent = calculationResult + eventObject.target.textContent;
+     }
   }
-console.log(orderArray);
-
+  console.log(Array.from(orderArray));
 });
+
+function calculate(dataArray){
+  if(dataArray.length !== 3) {
+    return;
+  }
+
+  switch(dataArray[1]){
+    case '+':
+      return dataArray[0] + dataArray[2];
+    case '-':
+      return dataArray[0] - dataArray[2];
+    case '/':
+      return dataArray[0] / dataArray[2];
+    case '*':
+      return dataArray[0] * dataArray[2];
+  }
+}
+
 
 
